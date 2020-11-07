543121385442c72cf1637f2333e7a8969ccf01a0
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-11-06 21:53:10 -0500

    fixed typo, added cats

--- 00-benchmark-scripts/2-revo-script.R
+++ 00-benchmark-scripts/2-revo-script.R
@@ -10,24 +10,29 @@ n <- 5000
 A <- matrix (runif (m*n),m,n)
 
 # Matrix multiply
+cat("Matrix multiply\n")
 system.time (B <- crossprod(A))
 
 # Cholesky Factorization
+cat("Cholesky Factorization\n")
 system.time (C <- chol(B))
 
-# Singular Value Deomposition
+# Singular Value Decomposition
+cat("Singular Value Decomposition\n")
 m <- 10000
 n <- 2000
 A <- matrix (runif (m*n),m,n)
 system.time (S <- svd (A,nu=0,nv=0))
 
 # Principal Components Analysis
+cat("Principal Components Analysis\n")
 m <- 10000
 n <- 2000
 A <- matrix (runif (m*n),m,n)
 system.time (P <- prcomp(A))
 
 # Linear Discriminant Analysis
+cat("Linear Discriminant Analysis\n")
 library('MASS')
 g <- 5
 k <- round (m/2)
